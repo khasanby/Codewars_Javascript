c5e57a96306cb8fb07a1426f2af4a108c8961246
khasanby <k.kholmatov@student.inha.uz>
2023-02-05 09:16:13 +0500

    Add files via upload

--- Find the Unique Number.js
+++ Find the Unique Number.js
@@ -1,20 +1,5 @@
-var encryptThis = function(text) {
-  if(text == '') {
-    return null;  
-  }
-  
-  let stringResults = [];
-  let strSplitted = text.split(' ');
-  strSplitted.forEach(str => {
-    let charsOfstr = [...str];
-    charsOfstr[0] = charsOfstr[0].charCodeAt(0);
-    if(charsOfstr.length >= 2) {
-      let second = charsOfstr[1];
-      let last = charsOfstr[charsOfstr.length-1];
-      charsOfstr[1] = last;
-      charsOfstr[charsOfstr.length-1] = second;
-    }
-    stringResults.push(charsOfstr.join(''));
-  });
-  return stringResults.join(' ');
+function findUniq(arr) {
+  return arr.find(
+    (item) => arr.indexOf(item) === arr.lastIndexOf(item)
+  );
 }
