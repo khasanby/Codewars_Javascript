fd766419b6e0b15113cfba510d0f5c4d2da750c7
khasanby <k.kholmatov@student.inha.uz>
2023-02-06 11:27:14 +0500

    Update Encyript this.js

--- Encyript this.js
+++ Encyript this.js
@@ -9,6 +9,11 @@
 //The first letter must be converted to its ASCII code.
 //The second letter must be switched with the last letter
 //Keepin' it simple: There are no special characters in the input.
+//Examples:
+//encryptThis("Hello") === "72olle"
+//encryptThis("good") === "103doo"
+//encryptThis("hello world") === "104olle 119drlo"
+
 // Link: https://www.codewars.com/kata/5848565e273af816fb000449
 
 var encryptThis = function(text) {
