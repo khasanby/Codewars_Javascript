28b76dcb8e899e7bf86bb7d003aac6371a8cabdb
khasanby <k.kholmatov@student.inha.uz>
2023-02-06 11:17:49 +0500

    Update Encyript this.js

--- Encyript this.js
+++ Encyript this.js
@@ -1,3 +1,16 @@
+//DESCRIPTION:
+//Acknowledgments:
+//I thank yvonne-liu for the idea and for the example tests :)
+//Description:
+//Encrypt this!
+//You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+//Your message is a string containing space separated words.
+//You need to encrypt each word in the message using the following rules:
+//The first letter must be converted to its ASCII code.
+//The second letter must be switched with the last letter
+//Keepin' it simple: There are no special characters in the input.
+// Link: https://www.codewars.com/kata/5848565e273af816fb000449
+
 var encryptThis = function(text) {
   if(text == '') {
     return null;  
