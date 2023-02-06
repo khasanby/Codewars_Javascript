7d16c616e091e339c17feaa433d73e88bc36735a
khasanby <k.kholmatov@student.inha.uz>
2023-02-06 11:15:12 +0500

    Update Array.diff.js

--- Array.diff.js
+++ Array.diff.js
@@ -1,3 +1,7 @@
+//DESCRIPTION:
+//Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+//It should remove all values from list a, which are present in list b keeping their order.
+// Link: https://www.codewars.com/kata/523f5d21c841566fde000009
 function arrayDiff(a, b) {
   if(a.length == 0 || b.length == 0) return a;
   
