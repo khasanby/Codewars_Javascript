b03d8bbfe403671282cd0aa42588da46f3c2e724
khasanby <k.kholmatov@student.inha.uz>
2023-02-06 11:16:38 +0500

    Update Change two-dimensional array.js

--- Change two-dimensional array.js
+++ Change two-dimensional array.js
@@ -1,3 +1,8 @@
+//DESCRIPTION:
+//Function receive a two-dimensional square array of random integers. On the main diagonal, all the negative integers must be changed to 0, while the others must be changed to 1 (Note: 0 is considered non-negative, here).
+//(You can mutate the input if you want, but it is a better practice to not mutate the input)
+// Link: https://www.codewars.com/kata/581214d54624a8232100005f
+
 function matrix(array) {
   if(array.length == 0) return null;
   
