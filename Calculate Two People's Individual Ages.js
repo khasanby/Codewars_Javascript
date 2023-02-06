0fb02312e1f886fd5367af5353b906218c112865
khasanby <k.kholmatov@student.inha.uz>
2023-02-06 11:15:48 +0500

    Update Calculate Two People's Individual Ages.js

--- Calculate Two People's Individual Ages.js
+++ Calculate Two People's Individual Ages.js
@@ -1,3 +1,10 @@
+//DESCRIPTION:
+//Create a function that takes in the sum and age difference of two people, calculates their individual ages, and returns a pair of values (oldest age first) if those exist or null/None if:
+//sum < 0
+//difference < 0
+//Either of the calculated ages come out to be negative
+// Link: https://www.codewars.com/kata/58e0bd6a79716b7fcf0013b1
+
 function getAges(sum,difference){
   if(sum <= 0 || difference <= 0 || sum < difference) return null;  
   let b = (sum-difference)/2;
